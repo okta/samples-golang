7aa2ed174dd78b09028970cbd5f9546e59cea1e7
Brian Retterer <bretterer@gmail.com>
2021-01-15 11:40:47 -0500

    Add message stating server starting. Resolves #25

--- custom-login/main.go
+++ custom-login/main.go
@@ -36,6 +36,7 @@ func main() {
 	http.HandleFunc("/profile", ProfileHandler)
 	http.HandleFunc("/logout", LogoutHandler)
 
+	log.Print("server starting at localhost:8080 ... ")
 	err := http.ListenAndServe("localhost:8080", nil)
 	if err != nil {
 		log.Printf("the HTTP server failed to start: %s", err)
