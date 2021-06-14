02c6bf16f403ea4dfbd5f31a70adfa5af7d068d1
Mike Mondragon <mikemondragon@gmail.com>
2021-06-13 22:21:39 -0700

    Add tag numbers to scenarios. Finished 4.1.3

--- identity-engine/embedded-auth-with-sdk/harness/testing_harness_util.go
+++ identity-engine/embedded-auth-with-sdk/harness/testing_harness_util.go
@@ -326,6 +326,12 @@ func (th *TestHarness) fillsInSignUpEmail() error {
 	}
 	return th.fillsInFormValue(`input[name="email"]`, th.currentProfile.EmailAddress, th.waitForRegistrationForm)
 }
+func (th *TestHarness) fillsInInvalidSignUpEmail() error {
+	if th.currentProfile == nil {
+		return errors.New("test harness doesn't have a current profile")
+	}
+	return th.fillsInFormValue(`input[name="email"]`, "invalid-email-address-dot-com", th.waitForRegistrationForm)
+}
 
 func (th *TestHarness) fillsInSignUpPassword() error {
 	if th.currentProfile == nil {
@@ -358,7 +364,7 @@ func (th *TestHarness) matchErrorMessage(partialErrStr string) error {
 		}
 
 		if matched, _ := regexp.MatchString(partialErrStr, text); !matched {
-			return false, fmt.Errorf("expected to find error message with %q message", partialErrStr)
+			return false, fmt.Errorf("expected error message %q to match %q", text, partialErrStr)
 		}
 
 		return true, nil
@@ -373,6 +379,10 @@ func (th *TestHarness) seesAuthFailedErrorMessage() error {
 
 func (th *TestHarness) seesNoAccountErrorMessage() error {
 	return th.matchErrorMessage("There is no account with the Username")
+}
+
+func (th *TestHarness) seesErrorMessage(message string) error {
+	return th.matchErrorMessage(message)
 }
 
 func (th *TestHarness) isLoggedOut() error {
